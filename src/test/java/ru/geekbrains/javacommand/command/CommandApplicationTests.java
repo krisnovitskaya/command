22c04377f273e0545de1728a47b9fc70312439b4
Кристина <voitage@yandex.ru>
2021-03-12 14:22:07 +0300

    init project structure

---
+++ src/test/java/ru/geekbrains/javacommand/command/CommandApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.javacommand.command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
