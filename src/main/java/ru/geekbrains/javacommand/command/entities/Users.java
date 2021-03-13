9ba73176684e86eab3176c51ed57e30d71474225
Игорь Попович <popovichia@gmail.com>
2021-03-13 14:59:57 +0300

    Entity User is created.

---
+++ src/main/java/ru/geekbrains/javacommand/command/entities/Users.java
@@ -0,0 +1,77 @@
+/*
+ * License Headers.
+ */
+
+package ru.geekbrains.javacommand.command.entities;
+
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import org.hibernate.validator.constraints.Length;
+
+/** @author Igor Popovich, email: popovichia@gmail.com */
+@Entity()
+@Table(name = "users")
+public class Users {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "username", nullable = false, unique = true)
+  @Length(min = 3, max = 50)
+  private String userName;
+
+  @Column(name = "password", nullable = false)
+  @Length(min = 8, max = 64)
+  private String password;
+
+  @ManyToMany
+  @JoinTable(
+      name = "users_roles",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id"))
+  private Set<Role> listRoles;
+
+  public Users() {}
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Set<Role> getListRoles() {
+    return listRoles;
+  }
+
+  public void setListRoles(Set<Role> listRoles) {
+    this.listRoles = listRoles;
+  }
+}
