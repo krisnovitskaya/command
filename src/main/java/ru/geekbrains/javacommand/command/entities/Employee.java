f1e48431491bfa1218949728cc41c987de7926fb
Johnny3kk <some@mail.ru>
2021-03-13 18:26:23 +0300

    1 - класс Сотрудник

---
+++ src/main/java/ru/geekbrains/javacommand/command/entities/Employee.java
@@ -0,0 +1,90 @@
+package ru.geekbrains.javacommand.command.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "employees")
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String middle_name;
+
+    @Column
+    private String surname;
+
+    @ManyToOne(optional = false)
+    private Position position;
+
+    @ManyToOne(optional = false)
+    private Department department;
+
+    @ManyToOne(optional = false)
+    private User user;
+
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddle_name() {
+        return middle_name;
+    }
+
+    public void setMiddle_name(String middle_name) {
+        this.middle_name = middle_name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
