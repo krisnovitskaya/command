6c23bb6a8f63be14f53e321212be400bf5a37248
Andrei Krylov <krilovavit@mail.ru>
2021-03-14 09:39:10 +0300

    A001
    ErrandDetails Entity created

---
+++ src/main/java/ru/geekbrains/javacommand/command/entities/ErrandDetails.java
@@ -0,0 +1,51 @@
+package ru.geekbrains.javacommand.command.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import ru.geekbrains.javacommand.command.entities.Errand;
+import ru.geekbrains.javacommand.command.entities.Place;
+import ru.geekbrains.javacommand.command.entities.Employee;
+import ru.geekbrains.javacommand.command.entities.ErrandMatterType;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/** Created by Andrey Krylov 14.03.2021 */
+
+@Entity
+@Table(name = "errands_details")
+@Data
+@NoArgsConstructor
+public class ErrandDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(mappedBy = "errandDetails")
+    private Errand errand;
+
+    @ManyToOne
+    @JoinColumn(name = "matter_id", nullable = false)
+    private ErrandMatterType matter;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id", nullable = false)
+    private Place place;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    private Employee createdBy;
+
+    @ManyToOne
+    @JoinColumn(name = "confirmed_or_rejected_by", nullable = false)
+    private Employee confirmedOrRejectedBy;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+}
