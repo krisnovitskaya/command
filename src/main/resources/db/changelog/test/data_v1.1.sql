c64a08be556cddf97878d5818fed11ac96e3d2e4
owpk <vzvz4@mail.ru>
2021-03-18 19:07:05 +0900

    Task006 - merge liquibase Task004
    1. Добалвена библеоткеа auth0 для работы с jwt, заменяет собой JwtResponse dto класс и содержит инструменты для парсинга и создания jwt токена
    
    2. Настройка security вынесена в application.yaml:
    security:
       authorization-path: /auth -- эндпоинт авторизации
       jwt:
         expiration-time: 24 -- время действия токена (в часач)
         secret-key: testing -- секретный ключ
         token-prefix: Bearer -- тип токена
         token-header: Authorization -- хидер авторизованного запроса
    
    3. Merge ветки с Task004 (liquibase),
    добавлено 3 юзера (см. resources/db/changelog/test/data_v1.1.sql)
    4. Добавлен юнит тест секьюрного контроллера
    5. Добавлен javadoc

--- src/main/resources/db/changelog/test/data_v1.1.sql
+++ src/main/resources/db/changelog/test/data_v1.1.sql
@@ -1,13 +1,23 @@
 -- username: admin password: DLda#|d7~c?Q (bcrypt hash)
 insert into errands.users(username, password)
 values
-    ('admin', '$2y$12$ThsVE9vmV/bRHpvX4fVKmuoFVR9uWWqjMDZLjmP/.9Qm9L1sWup5K');
+    ('admin', '$2y$12$ThsVE9vmV/bRHpvX4fVKmuoFVR9uWWqjMDZLjmP/.9Qm9L1sWup5K'),
+    ('user1', '$2y$12$ThsVE9vmV/bRHpvX4fVKmuoFVR9uWWqjMDZLjmP/.9Qm9L1sWup5K'),
+    ('user2', '$2y$12$ThsVE9vmV/bRHpvX4fVKmuoFVR9uWWqjMDZLjmP/.9Qm9L1sWup5K'),
+    ('user3', '$2y$12$ThsVE9vmV/bRHpvX4fVKmuoFVR9uWWqjMDZLjmP/.9Qm9L1sWup5K');
 
 insert into errands.roles(role_name)
 values
     ('ROLE_ADMIN'),
     ('ROLE_MASTER'),
     ('ROLE_EMPLOYEE');
+
+insert into errands.users_roles(user_id, role_id)
+values
+    (1, 1),
+    (1, 2),
+    (2, 3),
+    (3, 3);
 
 insert into errands.errands_status_types (status)
 values
