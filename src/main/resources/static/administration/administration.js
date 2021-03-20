43eb66e77830d84c5b21a3786d6b7e1f9520a54a
Johnny3kk <some@mail.ru>
2021-03-20 13:17:54 +0300

    1 - примерное устройство представления страницы адмистрирования.     Нужно протестировать работоспособность и в форме сотрудника     присоединить зависимые сущности.

--- src/main/resources/static/administration/administration.js
+++ src/main/resources/static/administration/administration.js
@@ -1,4 +1,42 @@
 angular.module('app').controller('administrationController', function ($scope, $http) {
     const contextPath = 'http://localhost:8989/errands';
 
+    $http.get("api/v1/employee/all")
+        .then(resp => {
+                $scope.emploeeList = resp.data;
+            },
+            resp => {
+                console.error(resp);
+            });
+
+    $scope.name = '';
+    $scope.middleName = '';
+    $scope.surname = '';
+
+    $scope.create = function (name, middleName, surname) {
+
+        $http.post("api/v1/employee/new", {'name': name, 'middleName' : middleName, 'surname' : surname})
+            .then(resp => {
+                    $scope.emploeeList.push(resp.data);
+                },
+                resp => {
+                    console.error(resp);
+                });
+
+        $scope.name = '';
+        $scope.middleName = '';
+        $scope.surname = '';
+    }
+
+    $scope.delete = function (employee) {
+        $http.delete("api/v1/employee/" + item.id)
+            .then(resp => {
+                    let ix = $scope.shoppingItemList.map(item => item.id).indexOf(item.id);
+                    $scope.shoppingItemList.splice(ix, 1);
+                },
+                resp => {
+                    console.error(resp);
+                });
+    }
+
 });
