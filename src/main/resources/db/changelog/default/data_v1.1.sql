1773ef8079ae7a6ce8d65a2d1f2a3048b00420b1
Игорь Попович <popovichia@gmail.com>
2021-03-20 20:53:42 +0300

    Task-9. Fix application.yaml, SQL-scripts.

--- src/main/resources/db/changelog/default/data_v1.1.sql
+++ src/main/resources/db/changelog/default/data_v1.1.sql
@@ -1,1 +1,23 @@
--- Default init database data
+-- Test init database data
+-- username: admin password: DLda#|d7~c?Q (bcrypt hash)
+insert into errands.users(username, password)
+values
+    ('admin', '$2y$12$ThsVE9vmV/bRHpvX4fVKmuoFVR9uWWqjMDZLjmP/.9Qm9L1sWup5K');
+
+insert into errands.roles(role_name)
+values
+    ('ROLE_ADMIN'),
+    ('ROLE_MASTER'),
+    ('ROLE_EMPLOYEE');
+
+insert into errands.errands_status_types (status)
+values
+    ('REQUESTED'),      
+    ('CONFIRMED'),      
+    ('REJECTED');
+
+insert into errands.errands_matter_types (matter)
+values
+    ('LOCAL'), 
+    ('PRIVATE'),     
+    ('LONG');
