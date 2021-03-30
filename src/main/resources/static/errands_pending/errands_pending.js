354f8676be90617441c4e8e3aa0a5ad9da5a8cf7
Andrei Krylov <krilovavit@mail.ru>
2021-03-30 08:06:17 +0300

    intermediate point

--- src/main/resources/static/errands_pending/errands_pending.js
+++ src/main/resources/static/errands_pending/errands_pending.js
@@ -1,4 +1,42 @@
 angular.module('app').controller('errandsPendingController', function ($scope, $http) {
     const contextPath = 'http://localhost:8989/errands';
 
+    const PAGINATION_DIAPASON = 2;
+
+    $scope.fillTable = function (pageIndex = 1) {
+        $http({
+            url: contextPath + '/api/v1/errands/pending',
+            method: 'GET',
+            params: {
+                employee: $scope.filter ? $scope.filter.employee : null,
+                detailMatterType: $scope.filter ? $scope.filter.detailMatterType : null,
+                dateStart: $scope.filter ? $scope.filter.dateStart : null,
+                dateEnd: $scope.filter ? $scope.filter.dateEnd : null,
+                p: pageIndex
+            }
+        })
+            .then(function (response) {
+                $scope.ErrandsPage = response.data;
+                $scope.PaginationArray = $scope.generatePagesInd(pageIndex - PAGINATION_DIAPASON, pageIndex + PAGINATION_DIAPASON);
+            });
+    };
+
+    $scope.generatePagesInd = function(startPage, endPage) {
+        let arr = [];
+        for (let i = startPage; i < endPage + 1; i++) {
+            if (i > 0 && i <= $scope.ErrandsPage.totalPages)
+                arr.push(i);
+        }
+        return arr;
+    }
+
+    $scope.clearFilter = function () {
+        $scope.filter = null;
+        $scope.fillTable();
+    }
+
+    $(".extremum-click").click(function () {
+        $(this).siblings(".extremum-slide").slideToggle("slow");
+    });
+
 });
