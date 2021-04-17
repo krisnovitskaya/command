d0c589aa8e2793fbb913a3a53205a98f33e181b7
ElenaGromova <g777elena@gmail.com>
2021-04-17 13:41:14 +0600

    17.04.2021 - устранила ошибку сохранения командировки - добавила очищение формы после создания командировки - у errandsDetails убрала для confirmed_or_rejected_by проверку на not null (когда создает сотрудник, у него еще никем не подтверждена и не отклонена заявка) - изменила эндпоинты под вид  "get_current" вместо "getCurrent" - проверила сохранение командировки под мастером и простым сотрудником

---
+++ src/main/java/ru/geekbrains/javacommand/command/dtos/CreatedErrandDto.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.javacommand.command.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.geekbrains.javacommand.command.entities.ErrandDetails;
+import java.time.OffsetDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CreatedErrandDto {
+        private String statusType;
+        private Long employeeId;
+        private OffsetDateTime dateStart;
+        private OffsetDateTime dateEnd;
+        private Long matterId;
+        private Long placeId;
+        private String comment;
+        private Long createdById;
+        private Long confirmedOrRejectedBy;
+        @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+        private OffsetDateTime created;
+        @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+        private OffsetDateTime updated;
+        private boolean deleted;
+        private Long confirmedOrRejectedById;
+        private ErrandDetails errandDetails;
+}
